eeceb5a1cb8f7f923e2cf99b4601d3f429142bc8
Sofie Verschueren <verschueren.sofie@telenet.be>
2021-05-05 17:35:01 +0200

    commentaar toevoegen

--- Server_arduino_communication/src/main.cpp
+++ Server_arduino_communication/src/main.cpp
@@ -3,24 +3,23 @@
 #include <RH_RF95.h>
 #include <RHReliableDatagram.h>
 
-#define CLIENT_ADDRESS 2
 #define SERVER_ADDRESS 1
 
-//Verzend buffer
-const int lengthSendingBuffer= 30;
-char emptyChar[lengthSendingBuffer]= " "; 
-char sendingBuf[lengthSendingBuffer][20];  
-int indexBuf=0;
-int indexBuf_empty=0; 
+//Circular send buffer
+const int lengthSendingBuffer = 30;
+char emptyChar[lengthSendingBuffer] = " ";
+char sendingBuf[lengthSendingBuffer][20];
+int indexBuf = 0;
+int indexBuf_empty = 0;
 unsigned long time;
 
-//Test voor in de buffer 
-uint16_t liters1= 0;
-uint8_t from1= 2;  
+//First data for in the buffer
+uint16_t liters1 = 0;
+uint8_t from1 = 2;
 
 //Serial communication ISR
-String responseRPI = "";      // a String to hold incoming data
-bool stringComplete = false;  // whether the string is complete
+String responseRPI = "";     // a String to hold incoming data
+bool stringComplete = false; // whether the string is complete
 
 // Singleton instance of the radio driver
 RH_RF95 driver(6, 2);
@@ -29,78 +28,84 @@ RH_RF95 driver(6, 2);
 RHReliableDatagram manager(driver, SERVER_ADDRESS);
 
 //ISR communication with RPI
-void serialEvent() {
-  while (Serial.available()) {
+void serialEvent()
+{
+  while (Serial.available())
+  {
     // get the new byte:
     char inChar = (char)Serial.read();
     // add it to the inputString:
     responseRPI += inChar;
     // if the incoming character is a newline, set a flag so the main loop can
     // do something about it:
-    if (inChar == '\n') {
+    if (inChar == '\n')
+    {
       stringComplete = true;
     }
   }
 }
 
-void setup() 
+void setup()
 {
   // Dramco uno - enable 3v3 voltage regulator
- pinMode(8, OUTPUT);
-digitalWrite(8, HIGH);
-
+  pinMode(8, OUTPUT);
+  digitalWrite(8, HIGH);
 
   Serial.begin(9600);
-  while(!Serial) ; // Wait for serial port to be available
-  if(!manager.init())
+  while (!Serial)
+    ; // Wait for serial port to be available
+  if (!manager.init())
     Serial.println("init failed");
-    driver.setFrequency(868);
-  
-  // reserve 200 bytes for the inputString:
-  responseRPI.reserve(200);
-  //Serial.println("Setup complete");
+  driver.setFrequency(868);
 
-  sprintf(sendingBuf[indexBuf_empty],"%u:%u",from1,liters1);  //format two ints into character array
-  indexBuf_empty = (indexBuf_empty+1) % (lengthSendingBuffer); 
-  time= millis(); 
+  // Reserve 200 bytes for the inputString:
+  responseRPI.reserve(200);
+
+  // Placing first data in buffer
+  sprintf(sendingBuf[indexBuf_empty], "%u:%u", from1, liters1);
+  indexBuf_empty = (indexBuf_empty + 1) % (lengthSendingBuffer);
+  time = millis();
 }
 
 void loop()
 {
-    // print the string when a newline arrives:
-  if (stringComplete) {
-    if (responseRPI.compareTo("ACK")){
-      for(int i=0;i<lengthSendingBuffer;i++){
-      sendingBuf[indexBuf][i]=' ';
+  // When an acknowledgement has arrived, increment the index, and empty the buffer at the previous index.
+  if (stringComplete)
+  {
+    if (responseRPI.compareTo("ACK"))
+    {
+      for (int i = 0; i < lengthSendingBuffer; i++)
+      {
+        sendingBuf[indexBuf][i] = ' ';
       }
-      indexBuf = (indexBuf+1) % (lengthSendingBuffer); 
+      indexBuf = (indexBuf + 1) % (lengthSendingBuffer);
     }
     // clear the string:
     responseRPI = "";
     stringComplete = false;
   }
 
-  if ((indexBuf < indexBuf_empty ||(indexBuf==lengthSendingBuffer-1 && indexBuf != indexBuf_empty )) && millis()-time>1000){
+  //Each second if possible send serial data to RPI
+  if ((indexBuf < indexBuf_empty || (indexBuf == lengthSendingBuffer - 1 && indexBuf != indexBuf_empty)) && millis() - time > 1000)
+  {
     Serial.print(indexBuf);
-    Serial.print( "." );
-    Serial.println(sendingBuf[indexBuf]); 
-    time= millis(); 
-    }
+    Serial.print(".");
+    Serial.println(sendingBuf[indexBuf]);
+    time = millis();
+  }
 
-    
-    
-
+  //Receiving Lora communication, adding data to buffer
   if (manager.available())
   {
-    // Should be a message for us now   
+    // Should be a message for us now
     uint8_t buf_data[RH_RF95_MAX_MESSAGE_LEN];
     uint8_t len = sizeof(buf_data);
     uint8_t from;
     if (manager.recvfromAck(buf_data, &len, &from))
     {
-      uint16_t buf_merged= (buf_data[1] << 8) | buf_data[0];
-      sprintf(sendingBuf[indexBuf_empty],"%u:%u",from,buf_merged);  //format two ints into character array
-      indexBuf_empty = (indexBuf_empty+1) % (lengthSendingBuffer);
-    } 
+      uint16_t buf_merged = (buf_data[1] << 8) | buf_data[0];
+      sprintf(sendingBuf[indexBuf_empty], "%u:%u", from, buf_merged); //format two ints into character array
+      indexBuf_empty = (indexBuf_empty + 1) % (lengthSendingBuffer);
+    }
   }
 }
